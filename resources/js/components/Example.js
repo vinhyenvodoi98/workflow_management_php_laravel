d7cb147699d34de30c86935d81f43b89609e3a78
hoang <vinhyenvodoi98@gmail.com>
2019-09-20 01:10:12 +0700

    add : animation background

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Background from './Background';
+import './Example.css';
 
 export default class Example extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-
-                            <div className="card-body">I'm an example component!</div>
+            <div>
+                <Background />
+                <div className='content container'>
+                    <div className='row justify-content-center'>
+                        <div className='col-md-8'>
+                            <div className='card'>
+                                <div className='card-header'>Example Component</div>
+                                <div className='card-body'>I'm an example component!</div>
+                            </div>
                         </div>
                     </div>
                 </div>
