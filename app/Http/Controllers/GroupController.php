bc5002153dc5d94b26f5420cca16fdf78ebbd1dd
Tien <viettienha98@gmail.com>
2019-12-11 21:24:50 +0700

    Fix  small conflict error

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use App\GroupTask;
 use App\Group;
 use App\User;
