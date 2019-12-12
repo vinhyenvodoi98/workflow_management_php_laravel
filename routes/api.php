bf64b18d847be4e1a9b6a535ccb77e49d4070e4a
Tien <viettienha98@gmail.com>
2019-12-12 16:24:32 +0700

    Add indexLower

--- routes/api.php
+++ routes/api.php
@@ -34,8 +34,10 @@ Route::group(['middleware' => 'jwt.auth'], function () {
     Route::get('users', 'UserController@index');
     Route::get('users/currentUser/groups', 'UserController@currentUserGroups');
 
+
     // Group APIs
     Route::get('groups', 'GroupController@index');
+    Route::get('groups/lower', 'UserController@indexLowerTier');
     Route::post('groups', 'GroupController@create');
     Route::post('groups/group/update', 'GroupController@update');
     Route::post('groups/group/delete', 'GroupController@delete');
@@ -43,6 +45,7 @@ Route::group(['middleware' => 'jwt.auth'], function () {
     Route::post('groups/group/user/update', 'GroupController@updateUserTaskInGroup');
     Route::post('groups/group/user/delete', 'GroupController@deleteUserInGroup');
     Route::get('groups/{group_id}/users', 'GroupController@getUsersInfo');
+
 
     // Work APIs
     Route::get('user/groups/{group_id}/works', 'WorkController@index');
