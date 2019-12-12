c64f485a0269a728c61306b1634e90f767366ac1
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2019-12-12 11:08:59 +0700

    fix: department_pivots, target_pivots, work_pivots table database seeders' inconsistency

--- database/seeds/DepartmentPivotsTableSeeder.php
+++ database/seeds/DepartmentPivotsTableSeeder.php
@@ -12,19 +12,23 @@ class DepartmentPivotsTableSeeder extends Seeder
     public function run()
     {
         // factory(App\DepartmentPivot::class, 15)->create();
-        $children = App\Department::limit(15)->pluck('id');
+        $children = App\Department::limit(45)->pluck('id');
 
+        $parents = array();
         foreach ($children as $child) {
-            do
-                $parent = mt_rand(1, 20);
-            while ($parent == $child);
+            if (!in_array($child, $parents)) {
+                do
+                    $parent = mt_rand(1, 20);
+                while ($parent == $child);
+                array_push($parents, $parent);
 
-            DB::table('department_pivots')->insert(
-                [
-                    'parent_id' => $parent,
-                    'department_id' => $child,
-                ]
-            );
-        };
+                DB::table('department_pivots')->insert(
+                    [
+                        'parent_id' => $parent,
+                        'department_id' => $child,
+                    ]
+                );
+            };
+        }
     }
 }
--- database/seeds/WorkPivotsTableSeeder.php
+++ database/seeds/WorkPivotsTableSeeder.php
@@ -14,17 +14,21 @@ class WorkPivotsTableSeeder extends Seeder
         // factory(App\WorkPivot::class, 45)->create();
         $children = App\Work::limit(45)->pluck('id');
 
+        $parents = array();
         foreach ($children as $child) {
-            do
-                $parent = mt_rand(1, 50);
-            while ($parent == $child);
+            if (!in_array($child, $parents)) {
+                do
+                    $parent = mt_rand(1, 50);
+                while ($parent == $child);
+                array_push($parents, $parent);
 
-            DB::table('work_pivots')->insert(
-                [
-                    'parent_id' => $parent,
-                    'work_id' => $child,
-                ]
-            );
-        };
+                DB::table('work_pivots')->insert(
+                    [
+                        'parent_id' => $parent,
+                        'work_id' => $child,
+                    ]
+                );
+            };
+        }
     }
 }
